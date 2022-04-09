b9bbdca873f3950087975317713a88108135e13e
Kragba Loba Armel Moise <loba-armel-moise.kragba@etudiant.univ-rennes1.fr>
2022-04-09 13:41:27 +0200

    Add requetes HTTP

--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -1,55 +1,47 @@
 import {Injectable} from "@angular/core";
 import {FaceSnap} from "../models/face-snap.model";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {map, switchMap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FaceSnapsService{
-  faceSnaps: FaceSnap[] = [
-    {
-      id: 1,
-      title: "Archibald",
-      description: "Mon meilleur ami depuis tout petit",
-      createDate: new Date(),
-      snaps: 0,
-      imageUrl:'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      location:'Paris',
-    },
-    {
-      id: 2,
-      title: "Aklam",
-      description: "Mon idependance financiere",
-      createDate: new Date(),
-      snaps: 0,
-      imageUrl:'https://cdn.pixabay.com/photo/2017/03/27/14/56/auto-2179220_960_720.jpg'
-    },
-  ];
 
-  getAllFaceSnaps(): FaceSnap[]{
-    return this.faceSnaps;
+  constructor(private http: HttpClient) {
+  }
+  faceSnaps: FaceSnap[] = [];
+
+  getAllFaceSnaps(): Observable<FaceSnap[]>{
+    return this.http.get<FaceSnap[]>('http://localhost:3000/facesnaps');
   }
 
-  getFaceSnapById(faceSnapId: number): FaceSnap{
-    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
-    if(!faceSnap){
-      throw new Error('FaceSnap not found !');
-    }else{
-      return faceSnap;
-    }
+  getFaceSnapById(faceSnapId: number): Observable<FaceSnap>{
+    return this.http.get<FaceSnap>(`http://localhost:3000/facesnaps/${faceSnapId}`);
   }
 
-  snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void{
-    const faceSnap = this.getFaceSnapById(faceSnapId);
-    snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+  snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): Observable<FaceSnap>{
+    return this.getFaceSnapById(faceSnapId).pipe(
+      map(faceSnap => ({
+        ...faceSnap,
+        snaps: faceSnap.snaps +( snapType === 'snap' ? 1 : -1)
+      })),
+      switchMap(updateFaceSnap => this.http.put<FaceSnap>(`http://localhost:3000/facesnaps/${faceSnapId}`, updateFaceSnap))
+    );
   }
 
-  addFaceSnap(formValue: {title: string, description: string, imageUrl: string, location?: string}): void{
-    const faceSnap: FaceSnap = {
-      ...formValue,
-      createDate: new Date(),
-      snaps: 0,
-      id:this.faceSnaps[this.faceSnaps.length - 1].id + 1
-    };
-    this.faceSnaps.push(faceSnap);
+  addFaceSnap(formValue: {title: string, description: string, imageUrl: string, location?: string}): Observable<FaceSnap>{
+    return this.getAllFaceSnaps().pipe(
+      map(facesnaps => [...facesnaps].sort((a,b) => a.id - b.id)),
+      map(sortedFacesnaps => sortedFacesnaps[sortedFacesnaps.length -1]),
+      map(previousFcaeSnap => ({
+        ...formValue,
+        snaps: 0,
+        createDate: new Date(),
+        id: previousFcaeSnap.id + 1
+      })),
+      switchMap(newFaceSnap => this.http.post<FaceSnap>('http://localhost:3000/facesnaps', newFaceSnap))
+    );
   }
 }
--- src/app/single-face-snap/single-face-snap.component.ts
+++ src/app/single-face-snap/single-face-snap.component.ts
@@ -2,6 +2,8 @@ import {Component,OnInit} from '@angular/core';
 import {FaceSnap} from "../models/face-snap.model";
 import {FaceSnapsService} from "../services/face-snaps.service";
 import {ActivatedRoute} from "@angular/router";
+import {Observable} from "rxjs";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-single-face-snap',
@@ -10,7 +12,7 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class SingleFaceSnapComponent implements OnInit {
 
-  faceSnap! : FaceSnap;
+  faceSnap$! : Observable<FaceSnap>
   buttonTextt! : string;
 
   constructor(private faceSnapsService : FaceSnapsService,
@@ -20,16 +22,21 @@ export class SingleFaceSnapComponent implements OnInit {
   ngOnInit() {
     this.buttonTextt = 'Oh Snap!';
     const faceSnapId = +this.router.snapshot.params['id'];
-    this.faceSnap = this.faceSnapsService.getFaceSnapById(faceSnapId);
+    this.faceSnap$ = this.faceSnapsService.getFaceSnapById(faceSnapId);
   }
 
-  onSnap(){
+  onSnap(faceSnapId: number){
     if(this.buttonTextt === 'Oh Snap!'){
-      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
-      this.buttonTextt = 'Oops, unSnap!';
+      this.faceSnap$ = this.faceSnapsService.snapFaceSnapById(faceSnapId, 'snap').pipe(
+        tap(() =>
+          this.buttonTextt = 'Oops, unSnap!')
+      );
     }else{
-      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap');
-      this.buttonTextt = 'Oh Snap!';
+      this.faceSnap$ = this.faceSnapsService.snapFaceSnapById(faceSnapId, 'unsnap').pipe(
+        tap(() =>
+          this.buttonTextt = 'Oh Snap!'
+        )
+      );
     }
   }
 }
